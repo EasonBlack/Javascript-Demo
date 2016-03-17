95f8d4f957a4959885f271f49072045a0db73ed0
Eason_Xu <eason820720@163.com>
2016-03-17 14:34:32 +0800

    test

---
+++ js-cms/cross-content/src/box.js
@@ -0,0 +1,10 @@
+class Box {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString() {
+        return '('+this.x+','+this.y+')';
+    }
+}
+export default Box;
---
+++ js-cms/cross-content/src/main.js
@@ -0,0 +1,5 @@
+import Box from './box.js';
+import
+
+var body = document.querySelector('body');
+body.textContent = 'Good point: ' + new Box(1, 23);
